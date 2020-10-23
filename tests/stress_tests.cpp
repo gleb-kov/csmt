0ce739009cfcf047e60ab5140738f7ba90b78cff
Gleb Koveshnikov <kovg16@gmail.com>
2020-10-23 22:39:48 +0300

    stress added

--- tests/stress_tests.cpp
+++ tests/stress_tests.cpp
@@ -5,14 +5,95 @@
 #include <bitset>
 #include <functional>
 #include <random>
+#include <unordered_set>
 
-TEST(stress, comeback) {
-    Csmt<> tree;
-    constexpr size_t KEYS = 60;
+TEST(stress, spam_insert) {
+    constexpr size_t OPERATIONS = 10000;
 
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+
+    std::uniform_int_distribution<uint64_t> key_gen(0, std::numeric_limits<uint64_t>::max());
     std::function<std::string(size_t)> value_gen = [](size_t key_index) {
         return "VALUE" + std::to_string(key_index);
     };
+
+    Csmt<> tree;
+
+    for (size_t iter = 0; iter < OPERATIONS; iter++) {
+        uint64_t key = key_gen(generator);
+
+        EXPECT_NO_THROW(tree.insert(key, value_gen(key)));
+        ASSERT_TRUE(tree.contains(key));
+    }
+}
+
+TEST(stress, spam_erase) {
+    constexpr size_t OPERATIONS = 10000;
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+
+    std::uniform_int_distribution<uint64_t> key_gen(0, std::numeric_limits<uint64_t>::max());
+    std::function<std::string(size_t)> value_gen = [](size_t key_index) {
+        return "VALUE" + std::to_string(key_index);
+    };
+
+    Csmt<> tree;
+    std::unordered_set<uint64_t> keys;
+
+    for (size_t iter = 0; iter < OPERATIONS; iter++) {
+        uint64_t key = key_gen(generator);
+        while (keys.count(key)) {
+            key = key_gen(generator);
+        }
+        keys.insert(key);
+        tree.insert(key, value_gen(key));
+    }
+
+    for (uint64_t key: keys) {
+        EXPECT_NO_THROW(tree.erase(key));
+        ASSERT_FALSE(tree.contains(key));
+    }
+}
+
+TEST(stress, spam_membership_proof) {
+    constexpr size_t OPERATIONS = 10000;
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+
+    std::uniform_int_distribution<uint64_t> key_gen(0, std::numeric_limits<uint64_t>::max());
+    std::function<std::string(uint64_t)> value_gen = [](uint64_t key_index) {
+        return "VALUE" + std::to_string(key_index);
+    };
+
+    Csmt<> tree;
+    std::unordered_set<uint64_t> keys;
+
+    for (size_t iter = 0; iter < OPERATIONS; iter++) {
+        uint64_t key = key_gen(generator);
+        while (keys.count(key)) {
+            key = key_gen(generator);
+        }
+        keys.insert(key);
+        tree.insert(key, value_gen(key));
+        EXPECT_NO_THROW(auto proof = tree.membership_proof(key));
+    }
+
+    for (uint64_t key: keys) {
+        EXPECT_NO_THROW(auto proof = tree.membership_proof(key));
+    }
+}
+
+TEST(stress, comeback) {
+    constexpr size_t KEYS = 6000;
+
+    std::function<std::string(uint64_t)> value_gen = [](uint64_t key_index) {
+        return "VALUE" + std::to_string(key_index);
+    };
+
+    Csmt<> tree;
 
     for (size_t key_index = 0; key_index < KEYS; ++key_index) {
         tree.insert(key_index, value_gen(key_index));
@@ -42,15 +123,14 @@ TEST(stress, comeback) {
 }
 
 TEST(stress, pool) {
-    constexpr size_t KEYS = 70;
+    constexpr size_t KEYS = 100;
     constexpr size_t OPERATIONS = 10000;
 
     std::random_device random_device;
-    std::mt19937 generator;
+    std::mt19937 generator(random_device());
 
     std::uniform_int_distribution<> op_gen(0, 2);
     std::uniform_int_distribution<uint64_t> key_gen(0, KEYS - 1);
-
     std::function<std::string(uint64_t)> value_gen = [](uint64_t key_index) {
         return "VALUE" + std::to_string(key_index);
     };
