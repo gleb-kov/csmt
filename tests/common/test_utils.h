ce0350d3844805b02d418768efc7b04b196f3668
amathael <dan.io.oreshnikov@gmail.com>
2020-10-23 17:48:45 +0300

    fixed random generation in benchmark, added empty template args to stage_timer

--- tests/common/test_utils.h
+++ tests/common/test_utils.h
@@ -2,6 +2,8 @@
 #define CSMT_TEST_UTILS_H
 
 #include <string>
+#include <sstream>
+#include <random>
 
 struct IdentityHashPolicy {
     static std::string leaf_hash(std::string leaf_value) {
@@ -14,13 +16,25 @@ struct IdentityHashPolicy {
 };
 
 template<typename HP, typename HT, typename VT>
-inline bool look_for_key(const Csmt<HP, HT, VT> &tree, uint64_t key, const std::deque<HT> &proof = {}) {
+bool look_for_key(const Csmt<HP, HT, VT> &tree, uint64_t key, const std::deque<HT> &proof = {}) {
     bool empty = proof.empty();
     bool contains = tree.contains(key);
     if (empty == contains) {
         return false;
     }
     return tree.membership_proof(key) == proof;
+}
+
+inline std::string default_value_gen(uint64_t key_index) {
+    std::ostringstream s;
+    s << "VALUE" << key_index;
+    return s.str();
+}
+
+inline std::mt19937 rnd() {
+    static std::random_device random_device;
+    static std::mt19937 generator(random_device());
+    return generator;
 }
 
 #endif // CSMT_TEST_UTILS_H
